abc0a1378dcf8239d138aafe6da78cea535cf983
Jonathan Bowers <jonotron@gmail.com>
2015-03-02 15:03:35 -0800

    Calendar is interactive

--- angular-date-pickable.js
+++ angular-date-pickable.js
@@ -11,23 +11,49 @@ function jbDatePickableDirective () {
 
   function controller ($scope) {
     var vm = this;
+    vm.visibleDate = moment().startOf('month');
 
-    var start = moment().startOf('month');
-    var end = moment().endOf('month');
+    vm.setPrevMonth = setPrevMonth;
+    vm.setNextMonth = setNextMonth;
 
-    var range = moment().range(start, end);
+    vm.selectDate = selectDate;
 
-    var weeks = [];
-    range.by('weeks', function(week) {
-      var days = [];
-      moment().range(week.clone().startOf('week'), week.clone().endOf('week')).by('days', function(day) {
-        days.push(day.startOf('day').toDate()); 
-      });
-      weeks.push(days);
-    });
+    //
 
-    vm.weeks = weeks;
+    generateCalendar();
 
     return vm;
+
+    function setPrevMonth() {
+      vm.visibleDate = vm.visibleDate.add('month', -1).startOf('month'); 
+      generateCalendar();
+    }
+
+    function setNextMonth() {
+      vm.visibleDate = vm.visibleDate.add('month', 1).startOf('month'); 
+      generateCalendar();
+    }
+
+    function generateCalendar() {
+      var start = vm.visibleDate.clone().startOf('month');
+      var end = vm.visibleDate.clone().endOf('month');
+
+      var range = moment().range(start, end);
+
+      var weeks = [];
+      range.by('weeks', function(week) {
+        var days = [];
+        moment().range(week.clone().startOf('week'), week.clone().endOf('week')).by('days', function(day) {
+          days.push(day.startOf('day').toDate()); 
+        });
+        weeks.push(days);
+      });
+
+      vm.weeks = weeks;
+    }
+
+    function selectDate(date) {
+      console.log('date picked', date) 
+    }
   }
 }
